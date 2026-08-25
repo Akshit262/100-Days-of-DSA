e83991e651c168f8aaaf7d4874705969bea4dcf3
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-08-25 21:52:11 +0530

    Day_08_Power_Recursion.c

--- Day_08_Power_Recursion.c
+++ Day_08_Power_Recursion.c
@@ -1,7 +1,30 @@
+/*
+ * Day 08 - Power Using Recursion
+ *
+ * Problem:
+ * Given integers a and b, compute a^b using recursion
+ * without using the pow() function.
+ *
+ * Input:
+ * - Two space-separated integers a and b
+ *
+ * Output:
+ * - Print a raised to the power b
+ *
+ * Example:
+ * Input:
+ * 2 5
+ *
+ * Output:
+ * 32
+ *
+ * Explanation:
+ * 2^5 = 2 * 2 * 2 * 2 * 2 = 32
+ */
+
 #include <stdio.h>
 
 long long power(int a, int b) {
-    // Base case
     if (b == 0) {
         return 1;
     }
