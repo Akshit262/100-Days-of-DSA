551d144a078da1d54be642f39c08cf27b1ec5ddc
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-08-26 21:11:09 +0530

    Day_09_Reverse_String.c

--- Day_09_Reverse_String.c
+++ Day_09_Reverse_String.c
@@ -1,3 +1,32 @@
+/*
+ * Day 09 - Reverse a String
+ *
+ * Problem:
+ * A secret system stores code names in forward order.
+ * To display them in mirror format, transform the given
+ * code name so that its characters appear in the opposite order.
+ *
+ * Input:
+ * - Single line: a lowercase string containing only
+ *   alphabetic characters (no spaces)
+ *
+ * Output:
+ * - Print the transformed code name after reversing
+ *   the characters.
+ *
+ * Example:
+ * Input:
+ * hello
+ *
+ * Output:
+ * olleh
+ *
+ * Explanation:
+ * The first character moves to the last position,
+ * the second to the second-last, and so on until
+ * the entire string is reversed.
+ */
+
 #include <stdio.h>
 #include <string.h>
 
