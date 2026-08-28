9c7347952df1a22433e89c491dc3d6b4952df64e
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-08-28 22:06:14 +0530

    Day_11_Matrix_Addition.c

--- Day_11_Matrix_Addition.c
+++ Day_11_Matrix_Addition.c
@@ -1,3 +1,46 @@
+/*
+ * Day 11 - Matrix Addition
+ *
+ * Problem:
+ * Write a program to perform addition of two matrices
+ * having the same dimensions.
+ *
+ * The sum of two matrices is obtained by adding
+ * corresponding elements of the matrices.
+ *
+ * Input:
+ * - First line: two integers m and n representing the
+ *   number of rows and columns.
+ * - Next m lines: n integers each representing the
+ *   elements of the first matrix.
+ * - Next m lines: n integers each representing the
+ *   elements of the second matrix.
+ *
+ * Output:
+ * - Print the resultant matrix after addition.
+ * - Each row should be on a new line.
+ * - Elements should be separated by spaces.
+ *
+ * Example:
+ * Input:
+ * 3 3
+ * 1 2 3
+ * 4 5 6
+ * 7 8 9
+ * 9 8 7
+ * 6 5 4
+ * 3 2 1
+ *
+ * Output:
+ * 10 10 10
+ * 10 10 10
+ * 10 10 10
+ *
+ * Explanation:
+ * Each element of the result matrix is the sum of the
+ * corresponding elements from the two input matrices.
+ */
+
 #include <stdio.h>
 
 int main() {
