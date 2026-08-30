60b4477b40f98d6de77150d493352fa094b8887c
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-08-30 21:27:44 +0530

    Day_13_Boundary_to_Center.c

--- Day_13_Boundary_to_Center.c
+++ Day_13_Boundary_to_Center.c
@@ -1,3 +1,38 @@
+/*
+ * Day 13 - Boundary-to-Center Matrix Walk
+ *
+ * Problem:
+ * You are given a rectangular matrix of integers. Starting from
+ * the outer boundary, traverse the matrix in a clockwise manner
+ * and continue moving inward layer by layer until all elements
+ * are visited.
+ *
+ * Input:
+ * - First line: two integers r and c representing the number
+ *   of rows and columns
+ * - Next r lines: c integers each representing the matrix elements
+ *
+ * Output:
+ * - Print all visited elements in the order of traversal,
+ *   separated by spaces.
+ *
+ * Example:
+ * Input:
+ * 3 3
+ * 1 2 3
+ * 4 5 6
+ * 7 8 9
+ *
+ * Output:
+ * 1 2 3 6 9 8 7 4 5
+ *
+ * Explanation:
+ * The traversal begins along the top row, proceeds down the
+ * rightmost column, then moves across the bottom row in reverse,
+ * and finally goes up the leftmost column.
+ * The same pattern repeats for the inner submatrix.
+ */
+
 #include <stdio.h>
 
 int main() {
@@ -7,7 +42,6 @@ int main() {
 
     int matrix[r][c];
 
-    // Read matrix
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
             scanf("%d", &matrix[i][j]);
@@ -19,22 +53,18 @@ int main() {
     int left = 0;
     int right = c - 1;
 
-    // Spiral traversal
     while (top <= bottom && left <= right) {
 
-        // Move from left to right
         for (int j = left; j <= right; j++) {
             printf("%d ", matrix[top][j]);
         }
         top++;
 
-        // Move from top to bottom
         for (int i = top; i <= bottom; i++) {
             printf("%d ", matrix[i][right]);
         }
         right--;
 
-        // Move from right to left
         if (top <= bottom) {
             for (int j = right; j >= left; j--) {
                 printf("%d ", matrix[bottom][j]);
@@ -42,7 +72,6 @@ int main() {
             bottom--;
         }
 
-        // Move from bottom to top
         if (left <= right) {
             for (int i = bottom; i >= top; i--) {
                 printf("%d ", matrix[i][left]);
