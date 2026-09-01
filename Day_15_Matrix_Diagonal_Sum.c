83964a27423755d3c251b03dfdf7364b70796f44
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-01 21:29:37 +0530

    Day_15_Matrix_Diagonal_Sum.c

--- Day_15_Matrix_Diagonal_Sum.c
+++ Day_15_Matrix_Diagonal_Sum.c
@@ -1,3 +1,37 @@
+/*
+ * Day 15 - Matrix Diagonal Sum
+ *
+ * Problem:
+ * Given a matrix, calculate the sum of its primary diagonal
+ * elements.
+ *
+ * The primary diagonal consists of elements where the
+ * row index is equal to the column index.
+ *
+ * Input:
+ * - First line: two integers m and n
+ * - Next m lines: n integers each
+ *
+ * Output:
+ * - Print the sum of the primary diagonal elements.
+ *
+ * Example:
+ * Input:
+ * 3 3
+ * 1 2 3
+ * 4 5 6
+ * 7 8 9
+ *
+ * Output:
+ * 15
+ *
+ * Explanation:
+ * Primary diagonal elements are:
+ * 1, 5, 9
+ *
+ * Sum = 1 + 5 + 9 = 15
+ */
+
 #include <stdio.h>
 
 int main() {
