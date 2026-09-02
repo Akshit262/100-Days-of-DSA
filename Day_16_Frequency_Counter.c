c2146561d4e176c96d9cdce612fd3df3b52e0866
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-02 21:01:22 +0530

    Day_16_Frequency_Counter.c

--- Day_16_Frequency_Counter.c
+++ Day_16_Frequency_Counter.c
@@ -1,3 +1,33 @@
+/*
+ * Day 16 - Frequency Counter in Array
+ *
+ * Problem:
+ * Given an array of integers, count the frequency of each
+ * distinct element and print the result.
+ *
+ * Input:
+ * - First line: integer n (size of array)
+ * - Second line: n integers
+ *
+ * Output:
+ * - Print each distinct element followed by its frequency
+ *   in the format:
+ *   element:count
+ *
+ * Example:
+ * Input:
+ * 5
+ * 1 2 2 3 1
+ *
+ * Output:
+ * 1:2 2:2 3:1
+ *
+ * Explanation:
+ * 1 appears 2 times.
+ * 2 appears 2 times.
+ * 3 appears 1 time.
+ */
+
 #include <stdio.h>
 
 int main() {
