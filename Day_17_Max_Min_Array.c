54e0fadab71bf49ef78041a9c1949e4449ac579a
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-03 22:10:24 +0530

    Day_17_Max_Min_Array.c

--- Day_17_Max_Min_Array.c
+++ Day_17_Max_Min_Array.c
@@ -1,3 +1,27 @@
+/*
+ * Day 17 - Find Maximum and Minimum in an Array
+ *
+ * Problem:
+ * Write a program to find the maximum and minimum values
+ * present in a given array of integers.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n integers
+ *
+ * Output:
+ * - Print the maximum and minimum elements.
+ *
+ * Example:
+ * Input:
+ * 6
+ * 3 5 1 9 2 8
+ *
+ * Output:
+ * Max: 9
+ * Min: 1
+ */
+
 #include <stdio.h>
 
 int main() {
