2b6274fb13afafdcc6a73ebc75edd8960ed02087
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-05 21:33:37 +0530

    Day_19_Pair_Sum_Closest_to_Zero.c

--- Day_19_Pair_Sum_Closest_to_Zero.c
+++ Day_19_Pair_Sum_Closest_to_Zero.c
@@ -1,7 +1,33 @@
+/*
+ * Day 19 - Pair Sum Closest to Zero
+ *
+ * Problem:
+ * Given an array of integers, find two elements whose
+ * sum is closest to zero.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n space-separated integers
+ *
+ * Output:
+ * - Print the pair of elements whose sum is closest to zero.
+ *
+ * Example:
+ * Input:
+ * 5
+ * 1 60 -10 70 -80
+ *
+ * Output:
+ * -10 1
+ *
+ * Explanation:
+ * The sum of -10 and 1 is -9, which is closest to zero
+ * compared to all other possible pairs.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function used by qsort()
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
@@ -13,12 +39,10 @@ int main() {
 
     int arr[n];
 
-    // Read array
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Sort the array
     qsort(arr, n, sizeof(int), compare);
 
     int left = 0;
@@ -29,27 +53,24 @@ int main() {
 
     int bestSum = arr[left] + arr[right];
 
-    // Two-pointer approach
     while (left < right) {
         int sum = arr[left] + arr[right];
 
-        // Check if current sum is closer to zero
         if (abs(sum) < abs(bestSum)) {
             bestSum = sum;
             bestLeft = arr[left];
             bestRight = arr[right];
         }
 
-        // If sum is negative, increase it
         if (sum < 0) {
             left++;
         }
-        // If sum is positive, decrease it
+        
         else if (sum > 0) {
             right--;
         }
         else {
-            
+    
             bestLeft = arr[left];
             bestRight = arr[right];
             break;
