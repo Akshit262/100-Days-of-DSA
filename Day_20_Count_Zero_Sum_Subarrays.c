9a8d2f733ddc5a9921fe69a3a7f728d7567a189a
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-06 21:05:39 +0530

    Day_20_Count_Zero_Sum_Subarrays.c

--- Day_20_Count_Zero_Sum_Subarrays.c
+++ Day_20_Count_Zero_Sum_Subarrays.c
@@ -1,3 +1,37 @@
+/*
+ * Day 20 - Count Subarrays with Sum Zero
+ *
+ * Problem:
+ * Given an array of integers, count the number of subarrays
+ * whose sum is equal to zero.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n integers
+ *
+ * Output:
+ * - Print the count of subarrays having sum zero.
+ *
+ * Example:
+ * Input:
+ * 6
+ * 1 -1 2 -2 3 -3
+ *
+ * Output:
+ * 6
+ *
+ * Explanation:
+ * The zero-sum subarrays are:
+ * [1, -1]
+ * [2, -2]
+ * [3, -3]
+ * [1, -1, 2, -2]
+ * [2, -2, 3, -3]
+ * [1, -1, 2, -2, 3, -3]
+ *
+ * Total = 6
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +49,11 @@ int main() {
     long long count = 0;
     long long prefixSum = 0;
 
+    /*
+     * Store prefix sums and their frequencies.
+     * Since this is a beginner implementation, we use
+     * two arrays to store the prefix sums and their counts.
+     */
     long long prefix[n + 1];
     int frequency[n + 1];
 
@@ -37,7 +76,10 @@ int main() {
         }
 
         if (found != -1) {
-            
+            /*
+             * Every previous occurrence of the same prefix sum
+             * gives us one zero-sum subarray.
+             */
             count += frequency[found];
             frequency[found]++;
         } else {
