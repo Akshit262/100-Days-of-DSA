53b45980a4431fd095579ca8b903f368ece4542f
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-10 21:25:24 +0530

    Day_24_Delete_First_Occurrence.c

--- Day_24_Delete_First_Occurrence.c
+++ Day_24_Delete_First_Occurrence.c
@@ -1,3 +1,33 @@
+/*
+ * Day 24 - Delete First Occurrence of a Key
+ *
+ * Problem:
+ * Implement deletion of the first occurrence of a given key
+ * from a singly linked list using dynamic memory allocation.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n space-separated integers
+ * - Third line: integer key
+ *
+ * Output:
+ * - Print the linked list elements after deletion,
+ *   separated by spaces.
+ *
+ * Example:
+ * Input:
+ * 5
+ * 10 20 30 40 50
+ * 30
+ *
+ * Output:
+ * 10 20 40 50
+ *
+ * Explanation:
+ * Traverse the list, find the first node containing the key,
+ * and remove it by adjusting the previous node's next pointer.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -55,7 +85,7 @@ int main() {
         previous = current;
         current = current->next;
     }
-
+    
     current = head;
 
     while (current != NULL) {
