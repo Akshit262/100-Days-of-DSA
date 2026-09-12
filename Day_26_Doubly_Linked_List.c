68c60d56e7b8f8893d46e7c01c1fca521f61e5d8
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-12 21:31:37 +0530

    Day_26_Doubly_Linked_List.c

--- Day_26_Doubly_Linked_List.c
+++ Day_26_Doubly_Linked_List.c
@@ -1,3 +1,39 @@
+/*
+ * Day 26 - Doubly Linked List Insertion and Traversal
+ *
+ * Problem:
+ * Implement a doubly linked list using dynamic memory allocation.
+ *
+ * Each node contains:
+ * - data
+ * - next pointer
+ * - prev pointer
+ *
+ * Insert nodes sequentially and traverse the list
+ * from head using the next pointer.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n space-separated integers
+ *
+ * Output:
+ * - Print the linked list elements in forward order,
+ *   separated by spaces.
+ *
+ * Example:
+ * Input:
+ * 5
+ * 10 20 30 40 50
+ *
+ * Output:
+ * 10 20 30 40 50
+ *
+ * Explanation:
+ * Each node has data, next, and prev.
+ * Nodes are inserted sequentially and the list is
+ * traversed from head using the next pointer.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,7 +50,6 @@ int main() {
 
     struct Node *head = NULL;
     struct Node *tail = NULL;
-
 
     for (int i = 0; i < n; i++) {
         int value;
