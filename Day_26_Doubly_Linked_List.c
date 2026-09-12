470169bf9b73f43e30a55135dcae269713837c22
Akshit Chauhan <akshitchauhan2k6@gmail.com>
2026-09-12 21:29:48 +0530

    Day 26: Implement doubly linked list

---
+++ Day_26_Doubly_Linked_List.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    struct Node *head = NULL;
+    struct Node *tail = NULL;
+
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        struct Node *newNode = malloc(sizeof(struct Node));
+
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if (head == NULL) {
+            
+            head = newNode;
+            tail = newNode;
+        } else {
+            
+            tail->next = newNode;
+            newNode->prev = tail;
+
+            tail = newNode;
+        }
+    }
+
+    struct Node *current = head;
+
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+
+    current = head;
+
+    while (current != NULL) {
+        struct Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return 0;
+}
