7af57af910ddbcd96433ef2767f30f5b4568a6d4
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-14 22:17:05 +0530

    Day_28_Circular_Linked_List.c

--- Day_28_Circular_Linked_List.c
+++ Day_28_Circular_Linked_List.c
@@ -1,3 +1,35 @@
+/*
+ * Day 28 - Circular Linked List Creation and Traversal
+ *
+ * Problem:
+ * Implement a circular linked list using dynamic memory
+ * allocation.
+ *
+ * The last node's next pointer should point back to the head.
+ * Traverse the circular linked list starting from the head.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n space-separated integers
+ *
+ * Output:
+ * - Print the circular linked list elements starting from
+ *   head, separated by spaces.
+ *
+ * Example:
+ * Input:
+ * 5
+ * 10 20 30 40 50
+ *
+ * Output:
+ * 10 20 30 40 50
+ *
+ * Explanation:
+ * The last node's next points to the head.
+ * Traverse from head until returning to head to avoid
+ * an infinite loop.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
