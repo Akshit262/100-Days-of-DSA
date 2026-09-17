9343b77d2890407b13121a1528be7dd67751a262
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-17 21:16:09 +0530

    Day_31_Stack_Using_Array.c

--- Day_31_Stack_Using_Array.c
+++ Day_31_Stack_Using_Array.c
@@ -1,3 +1,39 @@
+/*
+ * Day 31 - Stack Implementation Using Array
+ *
+ * Problem:
+ * Implement a stack data structure using an array with
+ * the following operations:
+ * push, pop, and display.
+ *
+ * Input:
+ * - First line: integer n (number of operations)
+ * - Next n lines: operation type and value (if applicable)
+ *
+ * Operations:
+ * 1 value -> Push value onto the stack
+ * 2       -> Pop the top element
+ * 3       -> Display stack elements from top to bottom
+ *
+ * Output:
+ * - For display: print stack elements from top to bottom
+ * - For pop: print the popped element or "Stack Underflow"
+ *
+ * Example:
+ * Input:
+ * 5
+ * 1 10
+ * 1 20
+ * 3
+ * 2
+ * 3
+ *
+ * Output:
+ * 20 10
+ * 20
+ * 10
+ */
+
 #include <stdio.h>
 
 #define MAX 100
