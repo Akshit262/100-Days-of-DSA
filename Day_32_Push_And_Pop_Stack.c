fa2fbdaa1746c76b6066899ece6ffa4eeca0a913
Akshit chauhan <akshitchauhan2k6@gmail.com>
2026-09-18 21:17:03 +0530

    Day_32_Push_And_Pop_Stack.c

--- Day_32_Push_And_Pop_Stack.c
+++ Day_32_Push_And_Pop_Stack.c
@@ -1,3 +1,33 @@
+/*
+ * Day 32 - Push and Pop in Stack
+ *
+ * Problem:
+ * Implement push and pop operations on a stack and
+ * verify stack operations.
+ *
+ * Input:
+ * - First line: integer n
+ * - Second line: n integers to push
+ * - Third line: integer m (number of pops)
+ *
+ * Output:
+ * - Print remaining stack elements from top to bottom.
+ *
+ * Example:
+ * Input:
+ * 5
+ * 10 20 30 40 50
+ * 2
+ *
+ * Output:
+ * 30 20 10
+ *
+ * Explanation:
+ * Push: 10, 20, 30, 40, 50
+ * Pop twice: 50, 40
+ * Remaining stack: 30, 20, 10
+ */
+
 #include <stdio.h>
 
 #define MAX 1000
