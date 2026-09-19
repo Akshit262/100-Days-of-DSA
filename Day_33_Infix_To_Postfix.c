62944462d9edf18d65de7b4ad54657be9eae8973
Akshit Chauhan <akshitchauhan2k6@gmail.com>
2026-09-19 22:29:15 +0530

    Day 33: Convert infix to postfix

---
+++ Day_33_Infix_To_Postfix.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char value) {
+    stack[++top] = value;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char op) {
+    if (op == '^') {
+        return 3;
+    }
+
+    if (op == '*' || op == '/') {
+        return 2;
+    }
+
+    if (op == '+' || op == '-') {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    char infix[MAX];
+
+    scanf("%s", infix);
+
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+
+        if (isalnum(ch)) {
+            printf("%c", ch);
+        }
+
+        else if (ch == '(') {
+            push(ch);
+        }
+
+        else if (ch == ')') {
+            while (top != -1 && stack[top] != '(') {
+                printf("%c", pop());
+            }
+
+            if (top != -1) {
+                pop();  
+            }
+        }
+
+        else {
+            while (top != -1 &&
+                   stack[top] != '(' &&
+                   precedence(stack[top]) >= precedence(ch)) {
+                printf("%c", pop());
+            }
+
+            push(ch);
+        }
+    }
+
+    while (top != -1) {
+        printf("%c", pop());
+    }
+
+    return 0;
+}
